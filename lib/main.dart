fbd36a5b20f7bb4aa4e5d2f432c8c67cc01cf2e3
tonmoy054 <1820243@iub.edu.bd>
2019-11-01 00:09:21 +0600

    geolocation updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'google_map_services.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,15 +11,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
- // static const LatLng _center = const LatLng(45.521563, -122.677433);
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Map()
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Map()),
     );
   }
 }
@@ -28,30 +26,210 @@ class Map extends StatefulWidget {
 }
 
 class _MapState extends State<Map> {
+  GoogleMapController googleMapController;
+  GoogleMapServices googleMapServices = GoogleMapServices();
+
+  static LatLng _initialPosition;
+  LatLng _lastPosition = _initialPosition;
+
+  TextEditingController locationController = TextEditingController();
+  TextEditingController destinationController = TextEditingController();
+
+  final Set<Marker> _markers = {};
+  final Set<Marker> _polyLines = {};
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getUserLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return
+      _initialPosition == null? Container(
+        alignment: Alignment.center,
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ): Stack(
       children: <Widget>[
         GoogleMap(
-//              onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
-            target: LatLng(23.811152, 90.4331707),
+            target: _initialPosition,
             zoom: 15.0,
           ),
+          onMapCreated: onCreated,
+          myLocationEnabled: true,
+          mapType: MapType.normal,
+          compassEnabled: true,
+          markers: _markers,
+          onCameraMove: onCameraMove,
         ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(16,30,16,16),
-          child: Align(
-            alignment: Alignment.topRight,
-            child: FloatingActionButton(
-              onPressed: () => print('button pressed'),
-              materialTapTargetSize: MaterialTapTargetSize.padded,
-              backgroundColor: Colors.blueGrey[700],
-              child: const Icon(Icons.map, size: 36.0),
+        Positioned(
+          top: 50.0,
+          right: 15.0,
+          left: 15.0,
+          child: Container(
+            height: 50.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(3.0),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(1.0, 5.0),
+                    blurRadius: 10,
+                    spreadRadius: 3)
+              ],
+            ),
+            child: TextField(
+              cursorColor: Colors.black,
+              controller: locationController,
+              decoration: InputDecoration(
+                icon: Container(
+                  margin: EdgeInsets.only(left: 20, top: 5),
+                  width: 10,
+                  height: 10,
+                  child: Icon(
+                    Icons.location_on,
+                    color: Colors.orangeAccent,
+                  ),
+                ),
+                hintText: "Your Bus Stop",
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+              ),
             ),
           ),
         ),
+
+        Positioned(
+          top: 105.0,
+          right: 15.0,
+          left: 15.0,
+          child: Container(
+            height: 50.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(3.0),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(1.0, 5.0),
+                    blurRadius: 10,
+                    spreadRadius: 3)
+              ],
+            ),
+            child: TextField(
+              cursorColor: Colors.black,
+              controller: destinationController,
+              textInputAction: TextInputAction.go,
+//              onSubmitted: (value) {
+//                appState.sendRequest(value);
+//              },
+              decoration: InputDecoration(
+                icon: Container(
+                  margin: EdgeInsets.only(left: 20, top: 5),
+                  width: 10,
+                  height: 10,
+                  child: Icon(
+                    Icons.directions_bus,
+                    color: Colors.orangeAccent,
+                  ),
+                ),
+                hintText: "Destination Bus Stop",
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+              ),
+            ),
+          ),
+        ),
+//        Padding(
+//          padding: const EdgeInsets.fromLTRB(16, 30, 16, 16),
+//          child: Align(
+//            alignment: Alignment.topRight,
+//            child: FloatingActionButton(
+//              onPressed: onAddMarkerPressed,
+//              materialTapTargetSize: MaterialTapTargetSize.padded,
+//              backgroundColor: Colors.amberAccent[700],
+//              child: const Icon(Icons.location_on, size: 36.0),
+//            ),
+//          ),
+//        ),
       ],
     );
+  }
+
+  void onCreated(GoogleMapController controller) {
+    setState(() {
+      googleMapController = controller;
+    });
+  }
+
+  void onCameraMove(CameraPosition position) {
+    setState(() {
+      _lastPosition = position.target;
+    });
+  }
+
+  void onAddMarkerPressed() {
+    setState(() {
+      _markers.add(Marker(
+          markerId: MarkerId(_lastPosition.toString()),
+          position: _lastPosition,
+          infoWindow: InfoWindow(title: 'remember this place', snippet: 'good'),
+          icon: BitmapDescriptor.defaultMarker));
+      print(_markers);
+    });
+  }
+
+  List _decodePoly(String poly) {
+    var list = poly.codeUnits;
+    var lList = new List();
+    int index = 0;
+    int len = poly.length;
+    int c = 0;
+// repeating until all attributes are decoded
+    do {
+      var shift = 0;
+      int result = 0;
+
+      // for decoding value of one attribute
+      do {
+        c = list[index] - 63;
+        result |= (c & 0x1F) << (shift * 5);
+        index++;
+        shift++;
+      } while (c >= 32);
+      /* if value is negetive then bitwise not the value */
+      if (result & 1 == 1) {
+        result = ~result;
+      }
+      var result1 = (result >> 1) * 0.00001;
+      lList.add(result1);
+    } while (index < len);
+
+/*adding to previous value as done in encoding */
+    for (var i = 2; i < lList.length; i++) lList[i] += lList[i - 2];
+
+    print(lList.toString());
+
+    return lList;
+  }
+
+  void _getUserLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    List<Placemark> placeMark = await Geolocator().placemarkFromCoordinates(position.latitude, position.longitude);
+setState(() {
+  _initialPosition = LatLng(position.latitude, position.longitude);
+  locationController.text = placeMark[0].name;
+  print(placeMark[0].name);
+});
+
   }
 }
