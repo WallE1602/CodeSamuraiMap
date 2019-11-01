5b4d685b49c82eb13bbcc6f18600f396dddd630a
WallE1602 <1821545@iub.edu.bd>
2019-11-01 15:15:03 +0600

    Search Page Updated with latitude and longtitude

--- lib/SearchPage.dart
+++ lib/SearchPage.dart
@@ -9,14 +9,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home:  Scaffold(
-        backgroundColor: Colors.orangeAccent[400],
+        backgroundColor: Colors.blueGrey[600],
         body: SafeArea(
           child: Container(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               SizedBox(
-                height: 30,
+                height: 40,
+              ),
+              Text(
+                "Source",
+                style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),
               ),
               Container(
                 child: Container(
@@ -24,8 +28,8 @@ class MyApp extends StatelessWidget {
                   child: TextFormField(
                     textAlign: TextAlign.left,
                     decoration: InputDecoration(
-                      hintText: 'Enter Your Location',
-                      hintStyle: TextStyle(fontSize: 25),
+                      hintText: 'Enter Latitude',
+                      hintStyle: TextStyle(fontSize: 20),
                     ),
                   ),
                 ),
@@ -40,79 +44,68 @@ class MyApp extends StatelessWidget {
                     textAlign: TextAlign.left,
                     obscureText: true,
                     decoration: InputDecoration(
-                      hintText: 'Enter Your Destination',
-                      hintStyle: TextStyle(fontSize: 25),
+                      hintText: 'Enter Longtitude',
+                      hintStyle: TextStyle(fontSize: 20),
                     ),
                   ),
                 ),
               ),
 
               SizedBox(
-                height: 30,
+                height: 60,
               ),
-
+              Text(
+                "Destination",
+                style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),
+              ),
               Container(
-                height: 70,
-                width: 130,
-                padding: EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20)),
-                child: Center(
-                    child: Container(
-                      child: Text(
-                        'Find',
-                        style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),
-                      ),
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: TextFormField(
+                    textAlign: TextAlign.left,
+                    decoration: InputDecoration(
+                      hintText: 'Enter Latitude',
+                      hintStyle: TextStyle(fontSize: 20),
                     ),
                   ),
                 ),
-              ],
-          ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: TextFormField(
+                    textAlign: TextAlign.left,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Enter Longtitude',
+                      hintStyle: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 60,
+              ),
+
+            const SizedBox(height: 30),
+            RaisedButton(
+              color: Colors.white,
+             textColor: Colors.black,
+             onPressed: null,
+              child: const Text(
+                  'Find Route',
+                  style: TextStyle(fontSize: 30)
+              ),
+            ),
+            ],
+        ),
           ),
         ),
-      ),
+          ),
     );
   }
 }
-
-
-
-//  Card(
-//                color: Colors.white,
-//                margin: EdgeInsets.symmetric(vertical: 15.0,horizontal: 35.0),
-//                child: ListTile(
-//                  leading: Icon(
-//                    Icons.add_location,
-//                    color: Colors.black,
-//                  ),
-//                  title: Text(
-//                    'Enter Your Location',
-//                    style:  TextStyle(
-//                      fontFamily:'PTSerif',
-//                      color: Colors.black,
-//                      fontSize: 18.0,
-//                      letterSpacing: 2.5,
-//                    ),
-//                  ),
-//                ),
-//              ),
-//              Card(
-//                  color: Colors.white,
-//                  margin: EdgeInsets.symmetric(vertical: 5.0,horizontal: 35.0),
-//                  child: ListTile(
-//                    leading: Icon(
-//                      Icons.directions_bus,
-//                      color: Colors.black,
-//                    ),
-//                    title: Text(
-//                      'Enter Your Destination',
-//                      style:  TextStyle(
-//                        fontFamily:'PTSerif',
-//                        color: Colors.black,
-//                        fontSize: 16.0,
-//                        letterSpacing: 2.0,
-//                      ),
-//                    ),
-//                  )
-//              ),
